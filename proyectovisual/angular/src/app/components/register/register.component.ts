34cb4bd3668a90c4503c31d306df98fef2e25f54
AlejandroVMonreal <160094881+AlejandroVMonreal@users.noreply.github.com>
2024-10-09 22:11:55 -0600

    Feat implements form user
    Se implementa el formulario para registrar usuarios y se envia la informacion al servicio

--- proyectovisual/angular/src/app/components/register/register.component.ts
+++ proyectovisual/angular/src/app/components/register/register.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RegisterService } from '../../services/register.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  private httpService = inject(RegisterService)
+  formUser!:FormGroup
+
+  ngOnInit(): void {
+    this.formUser = new FormGroup({
+      nombre: new FormControl("",Validators.required),
+      apellido: new FormControl("",Validators.required),
+      telefono: new FormControl("",Validators.required),
+      correo: new FormControl("",[Validators.required,Validators.email]),
+      password: new FormControl("",Validators.required),
+    })
+  }
+
+  sendDataToService(){
+    console.log(this.formUser.value)
+    this.httpService.registerUser(this.formUser.value)
+  }
 
 }
