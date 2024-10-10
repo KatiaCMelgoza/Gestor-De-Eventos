7bc8e70abffcd8fc39a96cc77b1417a2e7e4bace
AlejandroVMonreal <160094881+AlejandroVMonreal@users.noreply.github.com>
2024-10-09 23:53:39 -0600

    config routing
    configuracion de la navegacion de la pagina

--- proyectovisual/angular/src/app/app.routes.ts
+++ proyectovisual/angular/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"",redirectTo:"inicio",pathMatch:"full"},
+    {path:"inicio", loadComponent: () => import("./components/banner/banner.component").then(m => m.BannerComponent)},
+    {path:"registro", loadComponent: () => import("./components/register/register.component").then(m => m.RegisterComponent)},
+
+    {path:"**", redirectTo:"inicio", pathMatch:"full"}
+    
+];
