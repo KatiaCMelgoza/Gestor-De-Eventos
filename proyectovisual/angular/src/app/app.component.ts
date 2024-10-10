f5f15d4e8d0e7822f6e75f6c0587c97c20e11f72
AlejandroVMonreal <160094881+AlejandroVMonreal@users.noreply.github.com>
2024-10-10 10:44:07 -0600

    add section
    se agrego el section

--- proyectovisual/angular/src/app/app.component.ts
+++ proyectovisual/angular/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { SectionComponent } from './components/section/section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RegisterComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, RegisterComponent, HeaderComponent, FooterComponent, SectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
