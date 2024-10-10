afcc3cc315634ce7cd451377651796f566d4c058
AlejandroVMonreal <160094881+AlejandroVMonreal@users.noreply.github.com>
2024-10-09 22:10:17 -0600

    Create class user
    Se crea la clase que se pretende usar para la informacion de los usuarios

---
+++ proyectovisual/angular/src/app/shared/classes/user/user.ts
@@ -0,0 +1,16 @@
+export class User {
+    private nombre:string
+    private apellido:string
+    private telefono:string
+    private correo:string
+    private password:string
+
+    constructor(data:any){
+        this.nombre = ""
+        this.apellido = ""
+        this.telefono = ""
+        this.correo = ""
+        this.password = ""
+    }
+
+}
