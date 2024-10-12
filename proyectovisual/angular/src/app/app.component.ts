74e42a9e3008d123102abd8ca78ac3af2b43dcee
AlejandroVMonreal <160094881+AlejandroVMonreal@users.noreply.github.com>
2024-10-12 13:20:32 -0600

    add router
    se agrego la ruta de inicio de sesion

--- proyectovisual/angular/src/app/app.component.ts
+++ proyectovisual/angular/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RegisterComponent } from './components/register/register.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SectionComponent } from './components/section/section.component';
+import { LoginComponent } from './components/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RegisterComponent, HeaderComponent, FooterComponent, SectionComponent],
+  imports: [RouterOutlet, RegisterComponent, HeaderComponent, FooterComponent, SectionComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- proyectovisual/angular/src/app/app.routes.ts
+++ proyectovisual/angular/src/app/app.routes.ts
@@ -5,7 +5,7 @@ export const routes: Routes = [
     {path:"",redirectTo:"inicio",pathMatch:"full"},
     {path:"inicio", loadComponent: () => import("./components/banner/banner.component").then(m => m.BannerComponent)},
     {path:"registro", loadComponent: () => import("./components/register/register.component").then(m => m.RegisterComponent)},
-
+    {path:"login", loadComponent:() => import("./components/login/login.component").then(m => m.LoginComponent)},
     {path:"**", redirectTo:"inicio", pathMatch:"full"}
     
 ];
