30bcde8a10b55463ab2402eb7449972d1b95914d
AlejandroVMonreal <160094881+AlejandroVMonreal@users.noreply.github.com>
2024-10-30 15:14:47 -0600

    add espacios components
    se agregaron los componentes de los espacios para eventos

--- proyectovisual/angular/src/app/app.routes.ts
+++ proyectovisual/angular/src/app/app.routes.ts
@@ -5,7 +5,10 @@ export const routes: Routes = [
     {path:"",redirectTo:"inicio",pathMatch:"full"},
     {path:"inicio", loadComponent: () => import("./components/banner/banner.component").then(m => m.BannerComponent)},
     {path:"registro", loadComponent: () => import("./components/register/register.component").then(m => m.RegisterComponent)},
+    {path:"proximos_eventos", loadComponent: () => import("./components/eventos-proximos/eventos-proximos.component").then(m => m.EventosProximosComponent)},
+    {path:"eventos_pasados", loadComponent: () => import("./components/eventos-pasados/eventos-pasados.component").then(m => m.EventosPasadosComponent)},
     {path:"login", loadComponent:() => import("./components/login/login.component").then(m => m.LoginComponent)},
+    {path:"solicitudes", loadComponent:() => import("./components/espacios/espacios.component").then(m => m.EspaciosComponent)},
     {path:"preguntas", loadComponent:() => import("./components/preguntas/preguntas.component").then(m => m.PreguntasComponent)},
     {path:"**", redirectTo:"inicio", pathMatch:"full"}
     
---
+++ proyectovisual/angular/src/app/components/espacios/espacios.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-espacios',
+  standalone: true,
+  imports: [],
+  templateUrl: './espacios.component.html',
+  styleUrl: './espacios.component.scss'
+})
+export class EspaciosComponent {
+
+}
