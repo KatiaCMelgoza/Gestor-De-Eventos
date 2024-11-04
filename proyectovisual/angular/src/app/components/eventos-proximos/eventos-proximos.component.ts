99248ea75737b3c9322a0c9791dfb4dfa9c24a47
AlejandroVMonreal <160094881+AlejandroVMonreal@users.noreply.github.com>
2024-11-04 17:01:31 -0600

    fix code
    se agregaron partes del codigo

--- proyectovisual/angular/src/app/components/eventos-proximos/eventos-proximos.component.ts
+++ proyectovisual/angular/src/app/components/eventos-proximos/eventos-proximos.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-eventos-proximos',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './eventos-proximos.component.html',
   styleUrl: './eventos-proximos.component.scss'
 })
