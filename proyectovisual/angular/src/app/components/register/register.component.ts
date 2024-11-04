5eb32736e21e871bb7a05dde5b8ac116f7a66b71
AlejandroVMonreal <160094881+AlejandroVMonreal@users.noreply.github.com>
2024-11-04 17:38:43 -0600

    import router
    se agrego la routa para los links

--- proyectovisual/angular/src/app/components/register/register.component.ts
+++ proyectovisual/angular/src/app/components/register/register.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RegisterService } from '../../services/register.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
