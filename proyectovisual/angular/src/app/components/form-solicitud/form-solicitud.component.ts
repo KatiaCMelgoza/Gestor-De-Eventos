334e79243c390180dcd872ea9aa6a37eadee8c82
AlejandroVMonreal <160094881+AlejandroVMonreal@users.noreply.github.com>
2024-11-11 15:18:58 -0600

    add component solicitud
    se agrego el componente de solicitud

---
+++ proyectovisual/angular/src/app/components/form-solicitud/form-solicitud.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-solicitud',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-solicitud.component.html',
+  styleUrl: './form-solicitud.component.scss'
+})
+export class FormSolicitudComponent {
+
+}
